e95fa0ba6115b3e411c46116f6809786435c0fb2
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2021-04-17 19:17:22 -0400

    Implemented zoom fitting

--- components/MainContent/TabContents/TabContent/TabDisplay/DisplayButtons/CameraButtons.vue
+++ components/MainContent/TabContents/TabContent/TabDisplay/DisplayButtons/CameraButtons.vue
@@ -40,6 +40,8 @@ export default {
   methods: {
 
     fitScreen() {
+      // Camera position
+
       let topLeft = { x: Infinity, y: Infinity }
       let bottomRight = { x: -Infinity, y: -Infinity }
 
@@ -56,7 +58,19 @@ export default {
         y: ((topLeft.y + bottomRight.y) / 2) || 0,
       }
 
-      this.tab.camera.zoom = 1
+
+
+      // Camera zoom
+
+      let tabRect = _app.getTabRect(this.tab.id)
+
+      console.log(topLeft.x - this.tab.camera.pos.x)
+      console.log(tabRect.width / 2)
+
+      this.tab.camera.zoom = Math.min(1, Math.min(
+        (Math.min(200, tabRect.width / 4) - tabRect.width / 2) / (topLeft.x - this.tab.camera.pos.x),
+        (Math.min(100, tabRect.height / 4) - tabRect.height / 2) / (topLeft.y - this.tab.camera.pos.y)
+      ))
     },
 
   },
