d4ba22950b04c4b2284997a7af3ddee5a5a39cfc
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2021-04-17 23:15:37 -0400

    Created properties component

---
+++ components/RightSidebar/Properties/Properties.vue
@@ -0,0 +1,69 @@
+<template>
+  <div v-if="activeNode != null">
+
+
+    <div>
+      <div class="mb-1 body-2 grey--text text--lighten-1">
+        Node name:
+      </div>
+
+      <v-text-field dense solo hide-details
+      class="body-2" v-model="activeNode.name">
+      </v-text-field>
+    </div>
+    
+
+    <div class="mt-4">
+      <div class="mb-1 body-2 grey--text text--lighten-1">
+        Node description:
+      </div>
+
+      <v-textarea dense solo hide-details class="body-2"
+      no-resize rows="2" v-model="activeNode.description">
+      </v-textarea>
+    </div>
+
+
+
+    <v-divider class="mt-5">
+    </v-divider>
+
+
+
+    <TableProperties :node="activeNode"></TableProperties>
+    <ModuleProperties :node="activeNode"></ModuleProperties>
+    <NodeProperties :node="activeNode"></NodeProperties>
+    <SQLProperties :node="activeNode"></SQLProperties>
+    
+    <SetOperationProperties :node="activeNode"></SetOperationProperties>
+    <JoinProperties :node="activeNode"></JoinProperties>
+
+    <FilterProperties :node="activeNode"></FilterProperties>
+    <TransformProperties :node="activeNode"></TransformProperties>
+    <DistinctProperties :node="activeNode"></DistinctProperties>
+    <SortProperties :node="activeNode"></SortProperties>
+    <ReduceProperties :node="activeNode"></ReduceProperties>
+    
+    <PivotProperties :node="activeNode"></PivotProperties>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+  computed: {
+
+    ..._vuex.mapGetters([
+      'activeNode',
+    ]),
+    
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- components/RightSidebar/RightSidebar.vue
+++ components/RightSidebar/RightSidebar.vue
@@ -21,49 +21,8 @@
     <div class="pa-5"
     style="flex: 1; height: 0; overflow-y: auto">
 
-      <div v-if="activeNode != null">
-
-
-        <div>
-          <div class="mb-1 body-2 grey--text text--lighten-1">Node name:</div>
-          <v-text-field dense solo hide-details
-          class="body-2" v-model="activeNode.name">
-          </v-text-field>
-        </div>
-        
-
-        <div class="mt-4">
-          <div class="mb-1 body-2 grey--text text--lighten-1">Node description:</div>
-          <v-textarea dense solo hide-details class="body-2"
-          no-resize rows="2" v-model="activeNode.description">
-          </v-textarea>
-        </div>
-
-
-
-        <v-divider class="mt-5">
-        </v-divider>
-
-
-
-        <TableProperties :node="activeNode"></TableProperties>
-        <ModuleProperties :node="activeNode"></ModuleProperties>
-        <NodeProperties :node="activeNode"></NodeProperties>
-        <SQLProperties :node="activeNode"></SQLProperties>
-        
-        <SetOperationProperties :node="activeNode"></SetOperationProperties>
-        <JoinProperties :node="activeNode"></JoinProperties>
-
-        <FilterProperties :node="activeNode"></FilterProperties>
-        <TransformProperties :node="activeNode"></TransformProperties>
-        <DistinctProperties :node="activeNode"></DistinctProperties>
-        <SortProperties :node="activeNode"></SortProperties>
-        <ReduceProperties :node="activeNode"></ReduceProperties>
-        
-        <PivotProperties :node="activeNode"></PivotProperties>
-        
-        
-      </div>
+      <Properties>
+      </Properties>
 
     </div>
 
@@ -81,10 +40,6 @@ export default {
     ..._vuex.mapFields({
       'rightSidebar': 'sidebars.right',
     }),
-
-    ..._vuex.mapGetters([
-      'activeNode',
-    ]),
 
   },
 
