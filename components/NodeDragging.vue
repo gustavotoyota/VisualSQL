f8c80715d6bcc90d9fbf00677fb6bb5f30f0b63c
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2021-06-08 18:45:59 -0400

    Fixed IE11 node dragging

--- components/NodeDragging.vue
+++ components/NodeDragging.vue
@@ -1,8 +1,7 @@
 <template>
 
   <div v-if="$state.nodeCreation.active && $state.nodeCreation.visible"
-  style="position: absolute; width: 100vw; height: 100vh;
-  pointer-events: none; overflow: hidden">
+  style="position: absolute; left: 0; top: 0; pointer-events: none">
     <v-btn style="position: absolute; z-index: 999;
     min-width: 36px; transform: translate(-50%, -50%)" width="0"
     :style="{ left: `${$state.pointer.pagePos.x}px`, top: `${$state.pointer.pagePos.y}px` }">
