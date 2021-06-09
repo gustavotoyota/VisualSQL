e2773931c8062579414f1975719972a7d59175d6
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2021-06-08 22:30:36 -0400

    Implemented single-object vuex mapping helpers

--- plugins/vuex.js
+++ plugins/vuex.js
@@ -4,10 +4,17 @@ import { mapFields } from 'vuex-map-fields'
 
 
 global._vuex = {
-  mapFields: mapFields,
-  
-  mapState: Vuex.mapState,
+
+  mapStates: Vuex.mapState,
   mapMutations: Vuex.mapMutations,
   mapGetters: Vuex.mapGetters,
   mapActions: Vuex.mapActions,
+  mapFields: mapFields,
+
+  mapState: (name) => Object.values(Vuex.mapState([name]))[0],
+  mapMutation: (name) => Object.values(Vuex.mapMutations([name]))[0],
+  mapGetter: (name) => Object.values(Vuex.mapGetters([name]))[0],
+  mapAction: (name) => Object.values(Vuex.mapActions([name]))[0],
+  mapField: (path) => Object.values(mapFields([path]))[0],
+  
 }
