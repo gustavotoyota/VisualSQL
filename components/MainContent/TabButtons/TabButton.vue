318352c3ba5451d370a003be3ba284753cd8cbd0
Gustavu Toyota <gustavotakachitoyota@gmail.com>
2021-06-12 14:35:29 -0400

    Fixed tab close button

--- components/MainContent/TabButtons/TabButton.vue
+++ components/MainContent/TabButtons/TabButton.vue
@@ -3,7 +3,7 @@
 
     {{ module.name }}
 
-    <v-btn plain small rounded width="0" @click="closeTab"
+    <v-btn small rounded text width="0" @click="closeTab"
     style="position: relative; left: 6px; min-width: 28px">
       <v-icon dense>mdi-close</v-icon>
     </v-btn>
