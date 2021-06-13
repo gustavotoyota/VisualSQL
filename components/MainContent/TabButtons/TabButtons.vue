49ab1914372fde6119df8708cd279f13ed11c7b0
Gustavu Toyota <gustavotakachitoyota@gmail.com>
2021-06-13 19:39:45 -0400

    Improved sidebar toggling for small screens

--- components/MainContent/TabButtons/TabButtons.vue
+++ components/MainContent/TabButtons/TabButtons.vue
@@ -1,11 +1,11 @@
 <template>
-  <div style="flex: none; display: flex; background-color: #1E1E1E">
+  <div style="flex: none; display: flex; background-color: #1E1E1E; justify-content: center">
 
 
 
     <v-btn rounded style="flex: none; min-width: 0; width: 48px; height: 48px;
     border-top-left-radius: 0; border-bottom-left-radius: 0"
-    @click="$state.sidebars.left = !$state.sidebars.left">
+    @click="toggleLeftSidebar">
       <span style="position: relative; left: -2px; top: 1px">
         <v-icon v-if="$state.sidebars.left" dense>mdi-chevron-double-left</v-icon>
         <v-icon v-else dense>mdi-chevron-double-right</v-icon>
@@ -33,7 +33,7 @@
 
     <v-btn rounded style="flex: none; min-width: 0; width: 48px; height: 48px;
     border-top-right-radius: 0; border-bottom-right-radius: 0"
-    @click="$state.sidebars.right = !$state.sidebars.right">
+    @click="toggleRightSidebar">
       <span style="position: relative; left: 2px; top: 1px">
         <v-icon v-if="$state.sidebars.right" dense>mdi-chevron-double-right</v-icon>
         <v-icon v-else dense>mdi-chevron-double-left</v-icon>
@@ -58,6 +58,26 @@ export default {
       set(value) {
         this.$state.project.tabs.currentId = this.$state.project.tabs.list[value || 0].id
       },
+    },
+
+  },
+
+
+
+  methods: {
+
+    toggleLeftSidebar() {
+      this.$state.sidebars.left = !this.$state.sidebars.left
+
+      if (this.$state.sidebars.left && innerWidth < 696)
+        this.$state.sidebars.right = false
+    },
+    
+    toggleRightSidebar() {
+      this.$state.sidebars.right = !this.$state.sidebars.right
+
+      if (this.$state.sidebars.right && innerWidth < 696)
+        this.$state.sidebars.left = false
     },
 
   },
