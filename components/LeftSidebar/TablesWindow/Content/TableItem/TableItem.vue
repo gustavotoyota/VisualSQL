0893d9576c5c16861742fb1135c4cc36fee9edf5
Gustavu Toyota <gustavotakachitoyota@gmail.com>
2021-06-20 00:40:02 -0400

    Removed edit button for tables

--- components/LeftSidebar/TablesWindow/Content/TableItem/TableItem.vue
+++ components/LeftSidebar/TablesWindow/Content/TableItem/TableItem.vue
@@ -1,69 +1,54 @@
 <template>
-  <v-list-item @click="$emit('click')">
+  <TableDialog v-slot="{ on }" title="Edit table"
+  :name="table.name" :columns="table.columns"
+  @submit="Object.assign(table, $event)">
+
+    <v-list-item v-on="on">
 
 
 
-    <v-list-item-icon>
-      <v-icon>mdi-table</v-icon>
-    </v-list-item-icon>
-        
-    <v-list-item-content>
-      <v-list-item-title class="body-2"
-      style="word-break: break-all; white-space: normal">
-        {{ table.name }}
-      </v-list-item-title>
-    </v-list-item-content>
-        
-
-
-
-    <v-menu offset-y>
-
-      <template v-slot:activator="{ on, attrs }">
-
-        <v-btn icon class="ml-1"
-        style="position: relative; left: 6px;
-        min-width: 0; width: 28px; height: 28px"
-        v-bind="attrs" v-on="on">
-          <v-icon dense>mdi-dots-horizontal</v-icon>
-        </v-btn>
-
-      </template>
-      
-      <v-list dense width="150">
-
-        <!-- Edit table -->
-
-        <TableDialog v-slot="{ on }" title="Edit table"
-        :name="table.name" :columns="table.columns"
-        @submit="Object.assign(table, $event)">
+      <v-list-item-icon>
+        <v-icon>mdi-table</v-icon>
+      </v-list-item-icon>
           
-          <v-list-item v-on="on">
-
-            <v-list-item-icon style="margin-right: 10px">
-                <v-icon>mdi-square-edit-outline</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-title>Edit</v-list-item-title>
-
-          </v-list-item>
-
-        </TableDialog>
+      <v-list-item-content>
+        <v-list-item-title class="body-2"
+        style="word-break: break-all; white-space: normal">
+          {{ table.name }}
+        </v-list-item-title>
+      </v-list-item-content>
+          
 
 
 
-        <!-- Delete table -->
+      <v-menu offset-y>
 
-        <DeleteTable :table="table"/>
+        <template v-slot:activator="{ on, attrs }">
+
+          <v-btn icon class="ml-1"
+          style="position: relative; left: 6px;
+          min-width: 0; width: 28px; height: 28px"
+          v-bind="attrs" v-on="on">
+            <v-icon dense>mdi-dots-horizontal</v-icon>
+          </v-btn>
+
+        </template>
         
-      </v-list>
+        <v-list dense width="150">
 
-    </v-menu>
+          <DeleteTable :table="table"/>
+          
+        </v-list>
+
+      </v-menu>
 
 
 
 
-  </v-list-item>
+    </v-list-item>
+
+  </TableDialog>
+
 </template>
 
 <script>
