5749bb5bc51aae855d2cf726e0cd93e51be80ab1
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2021-06-23 17:18:23 -0400

    Added Oracle database

--- plugins/app/databases/databases.js
+++ plugins/app/databases/databases.js
@@ -1,5 +1,5 @@
 
-//import oracle from './oracle/oracle.js'
+import oracle from './oracle/oracle.js'
 import mySQL from './mysql/mysql.js'
 import sqlServer from './sql-server/sql-server.js'
 import postgres from './postgres/postgres.js'
@@ -9,16 +9,16 @@ import postgres from './postgres/postgres.js'
 export default {
 
   data: {
-    //'oracle': oracle,
+    'oracle': oracle,
     'mysql': mySQL,
     'sql-server': sqlServer,
     'postgres': postgres,
   },
 
   items: [
-    //{ text: 'Oracle', value: 'oracle' },
+    { text: 'Oracle (≥ 12c)', value: 'oracle' },
     { text: 'MySQL', value: 'mysql' },
-    { text: 'SQL Server', value: 'sql-server' },
+    { text: 'SQL Server (≥ 2012)', value: 'sql-server' },
     { text: 'PostgreSQL', value: 'postgres' },
   ],
   
