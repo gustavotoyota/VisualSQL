11c8d511ed87b1f9f30269c43c5b327d49fcd86d
Gustavu Toyota <gustavotakachitoyota@gmail.com>
2021-06-25 13:12:25 -0400

    Changed website title

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: `${title} | Free Online SQL Tool`,
+    title: `${title} | Online SQL Building Tool`,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
