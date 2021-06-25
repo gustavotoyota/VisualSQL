4ea57dec9c5378552a24840216984555aa013caf
Gustavu Toyota <gustavotakachitoyota@gmail.com>
2021-06-25 14:36:41 -0400

    Included Adcash meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
-      { hid: 'description', name: 'description', content: description }
+      { hid: 'description', name: 'description', content: description },
+      { name: 'a.validate.01', content: 'b205dd19ce230a2249e787a8dd186cff0d09' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
