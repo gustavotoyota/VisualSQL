7f375c8c25c40b341cd91c058f31d33b6e64b489
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2021-06-26 18:39:56 -0400

    Improved title

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: `${title} | Online SQL Building Tool`,
+    title: `${title} | Online Query Building Tool`,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
