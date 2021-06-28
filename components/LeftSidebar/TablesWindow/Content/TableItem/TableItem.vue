d98f71ed19b2e9a521e2da57bdffbdb2ead55296
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2021-06-27 21:21:58 -0400

    Fixed table dragging

--- components/LeftSidebar/TablesWindow/Content/TableItem/TableItem.vue
+++ components/LeftSidebar/TablesWindow/Content/TableItem/TableItem.vue
@@ -1,56 +1,58 @@
 <template>
-  <TableDialog v-slot="{ on }" title="Edit table"
-  :name="table.name" :columns="table.columns"
-  @submit="$commit('editTable', {
-    tableId: table.id,
-    data: $event,
-  })">
-
-    <v-list-item v-on="on">
+  <v-list-item @click.prevent="">
 
 
 
-      <v-list-item-icon>
-        <v-icon>mdi-table</v-icon>
-      </v-list-item-icon>
-          
-      <v-list-item-content>
+    <v-list-item-icon>
+      <v-icon>mdi-table</v-icon>
+    </v-list-item-icon>
+        
+
+        
+    <TableDialog v-slot="{ on }" title="Edit table"
+    :name="table.name" :columns="table.columns"
+    @submit="$commit('editTable', {
+      tableId: table.id,
+      data: $event,
+    })">
+
+      <v-list-item-content v-on="on">
         <v-list-item-title class="body-2"
         style="word-break: break-all; white-space: normal">
           {{ table.name }}
         </v-list-item-title>
       </v-list-item-content>
-          
 
-
-
-      <v-menu offset-y>
-
-        <template v-slot:activator="{ on, attrs }">
-
-          <v-btn icon class="ml-1"
-          style="position: relative; left: 6px;
-          min-width: 0; width: 28px; height: 28px"
-          v-bind="attrs" v-on="on">
-            <v-icon dense>mdi-dots-horizontal</v-icon>
-          </v-btn>
-
-        </template>
+    </TableDialog>
         
-        <v-list dense width="150">
-
-          <DeleteTable :table="table"/>
-          
-        </v-list>
-
-      </v-menu>
 
 
 
+    <v-menu offset-y>
 
-    </v-list-item>
+      <template v-slot:activator="{ on, attrs }">
 
-  </TableDialog>
+        <v-btn icon class="ml-1"
+        style="position: relative; left: 6px;
+        min-width: 0; width: 28px; height: 28px"
+        v-bind="attrs" v-on="on">
+          <v-icon dense>mdi-dots-horizontal</v-icon>
+        </v-btn>
+
+      </template>
+      
+      <v-list dense width="150">
+
+        <DeleteTable :table="table"/>
+        
+      </v-list>
+
+    </v-menu>
+
+
+
+
+  </v-list-item>
 
 </template>
 
