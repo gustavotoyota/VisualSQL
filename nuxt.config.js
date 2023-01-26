75c0ba0bc5b459741e0cd00eb1a50e04f5806d07
Gustavo Toyota <gustavottoyota@gmail.com>
2023-01-26 09:48:50 -0400

    Improve tagline

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: `${title} | Free Online SQL Tool`,
+    title: `${title} | Free Online Query Builder`,
     meta: [
       { charset: "utf-8" },
       {
