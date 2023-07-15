4d3004cb0225ba8e9351c9ca6c47cb3eb0e940c3
Gustavo Toyota <gustavottoyota@gmail.com>
2023-07-15 18:00:40 -0400

    Improve meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,9 @@
 import colors from "vuetify/es5/util/colors";
 
-const title = "VisualSQL";
+const title = "VisualSQL | Free Online Query Builder";
 const description =
   "VisualSQL is a free online tool for efficient SQL query building. Build SQL queries visually and avoid the inconvenient side of SQL with VisualSQL.";
+const url = "https://visualsql.net/";
 
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
@@ -10,17 +11,53 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: `${title} | Free Online Query Builder`,
+    title: title,
     meta: [
       { charset: "utf-8" },
       {
         name: "viewport",
         content:
-          "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
       },
-      { hid: "description", name: "description", content: description }
+      { hid: "description", name: "description", content: description },
+
+      // Open Graph
+
+      { hid: "og:type", property: "og:type", content: "website" },
+      { hid: "og:title", property: "og:title", content: title },
+      { hid: "og:url", property: "og:url", content: url },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content: description,
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "/meta-image.png",
+      },
+
+      // Twitter
+
+      {
+        hid: "twitter:card",
+        name: "twitter:card",
+        content: "summary_large_image",
+      },
+      { hid: "twitter:title", name: "twitter:title", content: title },
+      { hid: "twitter:url", name: "twitter:url", content: url },
+      {
+        hid: "twitter:description",
+        name: "twitter:description",
+        content: description,
+      },
+      {
+        hid: "twitter:image",
+        name: "twitter:image",
+        content: "/meta-image.png",
+      },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
@@ -37,7 +74,7 @@ export default {
     { src: "~/plugins/app/utils/global-mixin.js" },
     { src: "~/plugins/app/utils/utils.js" },
 
-    { src: "~/plugins/app/app.js" }
+    { src: "~/plugins/app/app.js" },
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -46,14 +83,14 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    "@nuxtjs/vuetify"
+    "@nuxtjs/vuetify",
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/pwa
     "@nuxtjs/pwa",
-    "@nuxtjs/gtm"
+    "@nuxtjs/gtm",
   ],
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
@@ -69,10 +106,10 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
@@ -80,24 +117,24 @@ export default {
 
   pwa: {
     icon: {
-      fileName: "pwa-icon.png"
+      fileName: "pwa-icon.png",
     },
     meta: {
-      name: title
+      name: "VisualSQL",
     },
     manifest: {
-      name: title,
-      short_name: title,
+      name: "VisualSQL",
+      short_name: "VisualSQL",
       description: description,
-      background_color: "#272727"
-    }
+      background_color: "#272727",
+    },
   },
 
   server: {
-    host: "0.0.0.0" // default: localhost
+    host: "0.0.0.0", // default: localhost
   },
 
   gtm: {
-    id: "GTM-KH4V429"
-  }
+    id: "GTM-KH4V429",
+  },
 };
